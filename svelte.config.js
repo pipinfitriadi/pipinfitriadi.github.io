a9f82cd540ab8495157c80bdb78313c66e32b846
Pipin Fitriadi <pipinfitriadi@gmail.com>
2024-07-17 19:14:30 +0700

    fix: Implement Sveltekit HTML Minifier
    Ref: https://github.com/ntsd/sveltekit-html-minifier

--- svelte.config.js
+++ svelte.config.js
@@ -7,17 +7,10 @@ Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 17 July 2024
 */
 
 import adapter from '@sveltejs/adapter-static';
+import htmlMinifierAdaptor from "sveltekit-html-minifier";
 
 export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: htmlMinifierAdaptor(adapter())
 	}
 };
