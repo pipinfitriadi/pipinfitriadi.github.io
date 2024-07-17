167bf2846280421c4ea5adda7e6c71b6b67f51d5
Pipin Fitriadi <pipinfitriadi@gmail.com>
2024-07-17 18:19:30 +0700

    feat: Init svelte

---
+++ src/app.d.ts
@@ -0,0 +1,21 @@
+/*
+Copyright (C) Pipin Fitriadi - All Rights Reserved
+
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 17 July 2024
+*/
+
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+export {};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<!-- 
+Copyright (C) Pipin Fitriadi - All Rights Reserved
+
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Pipin Fitriadi <pipinfitriadi@gmail.com>, 17 July 2024
+-->
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
