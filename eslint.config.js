b6a6a122772538a9ab4856563289f5ccce483411
Pipin Fitriadi <pipinfitriadi@gmail.com>
2024-07-19 11:15:43 +0700

    fix: eslint.config.js
    Issue '__APP_VERSION__' is not defined on `src/routes/+page.svelte`

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,8 @@ export default [
         languageOptions: {
             globals: {
                 ...globals.browser,
-                ...globals.node
+                ...globals.node,
+                __APP_VERSION__: 'readonly'
             }
         }
     },
